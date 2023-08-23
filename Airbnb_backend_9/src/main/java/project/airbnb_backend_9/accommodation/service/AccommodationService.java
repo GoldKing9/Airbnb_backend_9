d92346764c5cb93c430f6b2427be5c760165b29c
Jisoo Kim <ooSoo2Soo2oo@gmail.com>
2023-08-23 21:25:03 +0900

    숙박 등록, 수정, 삭제 이미지 제외하고 테스트까지 완료

--- Airbnb_backend_9/src/main/java/project/airbnb_backend_9/accommodation/service/AccommodationService.java
+++ Airbnb_backend_9/src/main/java/project/airbnb_backend_9/accommodation/service/AccommodationService.java
@@ -2,13 +2,17 @@ package project.airbnb_backend_9.accommodation.service;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.server.ResponseStatusException;
+import project.airbnb_backend_9.accommodation.dto.request.AccommodationEditRequestDTO;
 import project.airbnb_backend_9.accommodation.dto.request.AccommodationRequestDTO;
 import project.airbnb_backend_9.domain.Accommodation;
+import project.airbnb_backend_9.domain.Users;
 import project.airbnb_backend_9.repository.image.ImageRepository;
 import project.airbnb_backend_9.repository.accommodation.AccommodationRepository;
 import project.airbnb_backend_9.accommodation.dto.request.SearchRequest;
@@ -16,6 +20,7 @@ import project.airbnb_backend_9.accommodation.dto.response.AccommodationDataDto;
 import project.airbnb_backend_9.accommodation.dto.response.SearchResponse;
 import project.airbnb_backend_9.accommodation.dto.response.SingleAcmdResponse;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @Service
@@ -25,26 +30,77 @@ public class AccommodationService {
     private final AccommodationRepository accommodationRepository;
     private final ImageRepository imageRepository;
 
+    //숙박 등록
     @Transactional
-    public void saveAccommodation(AccommodationRequestDTO dto,
-                                  List<MultipartFile> images) {
+    public void saveAccommodation(AccommodationRequestDTO request,
+                                  List<MultipartFile> images,
+                                  Users users) {
         Accommodation accommodation = Accommodation.builder()
-                .mainAddress(dto.getMainAddress())
-                .detailAddress(dto.getDetailAddress())
-                .guest(dto.getGuest())
-                .bedroom(dto.getBedroom())
-                .bed(dto.getBed())
-                .bathroom(dto.getBathroom())
-                .acmdName(dto.getAcmdName())
-                .acmdDescription(dto.getAcmdDescription())
-                .price(dto.getPrice())
+                .mainAddress(request.getMainAddress())
+                .detailAddress(request.getDetailAddress())
+                .guest(request.getGuest())
+                .bedroom(request.getBedroom())
+                .bed(request.getBed())
+                .bathroom(request.getBathroom())
+                .acmdName(request.getAcmdName())
+                .acmdDescription(request.getAcmdDescription())
+                .price(request.getPrice())
+                .users(users)
                 .build();
 
-        // 이미지 넣어주는 부분도 추가해야 함
+        //이미지 등록 부분 추가해야 함
 
         accommodationRepository.save(accommodation);
-
     }
+
+    //숙박 수정
+    @Transactional
+    public void editAccommodation(Long accommodationId, AccommodationEditRequestDTO request, List<MultipartFile> newImages, Users users) {
+        //숙소 정보 조회, 없으면 예외 처리
+        Accommodation accommodation = accommodationRepository.findById(accommodationId)
+                .orElseThrow(() -> new EntityNotFoundException("해당하는 숙소가 없습니다. ID : " + accommodationId));
+
+        //숙소 소유자와 현재 사용자가 일치하는지 확인 (편집 권한 검사)
+        if (!accommodation.getUsers().getUserId().equals(users.getUserId())) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "이 숙소를 편집할 권한이 없습니다."); // 예외 처리 이렇게 해도 되나?
+        }
+
+        //숙소 정보 업데이트
+        accommodation.updateAccommodation(
+                request.getGuest(),
+                request.getBedroom(),
+                request.getBed(),
+                request.getBathroom(),
+                request.getAcmdName(),
+                request.getAcmdDescription(),
+                request.getPrice(),
+                users
+        );
+
+        //이미지 업데이트 부분 추가해야 함
+
+        //DB에 엔티티 저장
+        accommodationRepository.save(accommodation);
+    }
+
+    //숙박 삭제
+    @Transactional
+    public void deleteAccommodation(Long accommodationId, Users users) {
+        //숙소 정보 조회, 없으면 예외 처리
+        Accommodation accommodation = accommodationRepository.findById(accommodationId)
+                .orElseThrow(() -> new EntityNotFoundException("해당하는 숙소가 없습니다. ID : " + accommodationId));
+
+        //숙소 소유자와 현재 사용자가 일치하는지 확인 (삭제 권한 검사)
+        if (!accommodation.getUsers().getUserId().equals(users.getUserId())) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "이 숙소를 삭제할 권한이 없습니다.");
+        }
+
+        //이미지 삭제 부분 추가해야 함
+
+        //DB에 엔티티 저장
+        accommodationRepository.delete(accommodation);
+    }
+
   
     public SearchResponse search(Pageable pageable, SearchRequest request){
         PageImpl<AccommodationDataDto> accommodations = accommodationRepository.search(pageable, request);
--- Airbnb_backend_9/src/main/java/project/airbnb_backend_9/domain/Accommodation.java
+++ Airbnb_backend_9/src/main/java/project/airbnb_backend_9/domain/Accommodation.java
@@ -46,4 +46,17 @@ public class Accommodation {
         this.users = users;
     }
 
+    // 엔티티 자체가 자신의 상태를 수정하는데 필요한 행동을 수행 -> 데이터와 관련된 로직이 객체 내에 캡슐화 -> 객체 지향적
+    // 관련된 데이터와 로직이 하나의 단위로 묶임 -> 해당 메서드 내에서 필요한 데이터와 동작에 쉽게 접근하고 조작 -> 코드의 응집도 향상
+    public void updateAccommodation(Long guest, Long bedroom, Long bed, Long bathroom, String acmdName, String acmdDescription, Long price, Users users) {
+        this.guest = guest;
+        this.bedroom = bedroom;
+        this.bed = bed;
+        this.bathroom = bathroom;
+        this.acmdName = acmdName;
+        this.acmdDescription = acmdDescription;
+        this.price = price;
+        this.users = users;
+    }
+
 }
