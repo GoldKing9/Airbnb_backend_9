e748b50a5d2bf73ae328944574e5dffb76be6dad
Jisoo Kim <ooSoo2Soo2oo@gmail.com>
2023-08-28 15:41:46 +0900

    #24 병합 후 다시  테스트 예정

--- Airbnb_backend_9/src/main/java/project/airbnb_backend_9/repository/reservation/ReservationRepositoryImpl.java
+++ Airbnb_backend_9/src/main/java/project/airbnb_backend_9/repository/reservation/ReservationRepositoryImpl.java
@@ -26,9 +26,7 @@ public class ReservationRepositoryImpl implements ReservationRepositoryCustom{
     public PageImpl<HostReservationResponseDTO> findHostReservation(Pageable pageable, PrincipalDetails principalDetails) {
         /**
          * 1. 어디서 뭘 찾고 어디서 뭘 가져오는지 먼저 생각하기
-         * 2. 같은 accommodationId 같고 있는 예약 정보 reservation에서 accommodationId로 찾기
-         * 3. accommodationId 으로 같은 accommodationId 을 가지고 있는 acmd_image, reservation 찾기
-         * 4. 상태 체크
+         * 2. 상태 체크
          * - checkOut > LocalDate.now() && isDeleted == false, 예약중
          * - checkOut < LocalDate.now() && isDeleted == false, 이용완료
          * - isDeleted == true, 취소
@@ -60,12 +58,15 @@ public class ReservationRepositoryImpl implements ReservationRepositoryCustom{
                 .limit(pageable.getPageSize())
                 .fetch();
 
-        Long count = queryFactory.select(reservation.count())
+        // accommodation에 있는 users의 userId는 숙소 가진 호스트의 아이디!
+        // reservation에 있는 users의 userId는 게스트의 아이디!
+
+        Long total = queryFactory.select(reservation.count())
                 .from(reservation)
                 .join(accommodation).on(reservation.accommodation.accommodationId.eq(accommodation.accommodationId))
                 .where(accommodation.users.userId.eq(principalDetails.getUsers().getUserId()))
                 .fetchOne();
 
-        return new PageImpl<HostReservationResponseDTO>(hostReservationResponseDto, pageable, count);
+        return new PageImpl<HostReservationResponseDTO>(hostReservationResponseDto, pageable, total);
     }
 }
